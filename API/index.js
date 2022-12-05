c4ec8820989367b1fed13ca6a79be67624438158
MaxHusky <5143221+Max-Husky@users.noreply.github.com>
2022-12-05 07:35:04 -0600

    created schemas

---
+++ API/index.js
@@ -0,0 +1,35 @@
+const DBURL = '';
+
+const mongoose = require('mongoose');
+const express = require('express');
+
+mongoose.connect(DBURL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const DB = mongoose.connection;
+
+DB.model('Billing', new mongoose.Schema(
+  {
+    ownerId: {type: mongoose.SchemaTypes.ObjectId, required: true, ref: 'Owner'},
+    hoursStayed: {type: Number, required: true, default: 0},
+    hourRate: {type: Number, required: true, default: 20},
+    amountOwed: {type: Number, required: true, default: 0}
+  }
+));
+
+DB.model('Owner', new mongoose.Schema(
+  {
+    ownerAddress: {type: String, required: true},
+    ownerPhone: {type: string, required: true},
+    ownerEmail: {type: string, required: true},
+    ownerPet: [
+      {type: mongoose.Schema.ObjectId, ref: 'Pet'}
+    ]
+  }
+));
+
+DB.model('Pet', new mongoose.Schema(
+  {
+    petName: {type: String, required: true},
+    petType: {type: String, required: true}
+  }
+));
