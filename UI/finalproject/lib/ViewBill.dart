f0510afbe82b8d3ce7e2f76a381871aa333eb710
Matthew Pilgrim <xb6662gs@go.minnstate.edu>
2022-12-11 11:54:11 -0600

    More UI layout implemented

--- UI/finalproject/lib/ViewBill.dart
+++ UI/finalproject/lib/ViewBill.dart
@@ -6,25 +6,26 @@ import 'main.dart';
 // import 'editOwner.dart';
 
 class ViewBill extends StatefulWidget {
-  final String id, ownerId;
-  final int hoursStayed, hourRate, amountOwed;
+  //final String id, ownerId;
+  //final int hoursStayed, hourRate, amountOwed;
 
   final BillingApi api = BillingApi();
 
   ViewBill(
-      this.id, this.hoursStayed, this.hourRate, this.amountOwed, this.ownerId,
+      //this.id, this.hoursStayed, this.hourRate, this.amountOwed, this.ownerId,
       {super.key});
 
   @override
-  State<ViewBill> createState() =>
-      _ViewBillState(id, hoursStayed, hourRate, amountOwed, ownerId);
+  State<ViewBill> createState() => _ViewBillState(
+      //id, hoursStayed, hourRate, amountOwed, ownerId
+      );
 }
 
 class _ViewBillState extends State<ViewBill> {
-  final String id, ownerId;
-  final int hoursStayed, hourRate, amountOwed;
+  //final String id, ownerId;
+  //final int hoursStayed, hourRate, amountOwed;
   List owners = [];
-  bool _loaded = false;
+  bool _loaded = true;
 
   void initState() {
     super.initState();
@@ -37,7 +38,9 @@ class _ViewBillState extends State<ViewBill> {
   }
 
   _ViewBillState(
-      this.id, this.hoursStayed, this.hourRate, this.amountOwed, this.ownerId);
+      //this.id, this.hoursStayed, this.hourRate, this.amountOwed, this.ownerId
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,15 +52,29 @@ class _ViewBillState extends State<ViewBill> {
               ? Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                      Container(padding: const EdgeInsets.all(10)),
-                      const Text("Bill date",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 40)),
-                      Container(padding: const EdgeInsets.all(2)),
-                      Text("Invoice ID", style: TextStyle(fontSize: 25))
-                    ])
+                    Container(padding: const EdgeInsets.all(5)),
+                    Container(
+                        padding: const EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 10, color: Colors.grey),
+                        ),
+                        child: Text("Invoice Data",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold))),
+                    Container(padding: const EdgeInsets.all(5)),
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          border:
+                              Border.all(width: 10, color: Colors.lightGreen)),
+                      child: Text(
+                        "Owner Data",
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                )
               : Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
@@ -66,6 +83,13 @@ class _ViewBillState extends State<ViewBill> {
                           style: TextStyle(fontSize: 20)),
                       const CircularProgressIndicator()
                     ])),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.home),
+          onPressed: () => {
+                Navigator.pop(context),
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MyHomePage())),
+              }),
     );
   }
 }
